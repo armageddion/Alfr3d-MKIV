6d4a3d93d0aba5ab84f971f958438ef972a231b3
armageddion <armageddion@gmail.com>
2017-02-19 21:25:33 -0500

    bug fix

--- utilities/userClass.py
+++ utilities/userClass.py
@@ -219,6 +219,9 @@ class User:
 		db = client['Alfr3d_DB']
 		devicesCollection = db['devices']
 		usersCollection = db['users']
+		collection_env = db['environment']
+
+		cur_env = collection_env.find_one({"name":socket.gethostname()})
 
 		# cycle through all users
 		for user in usersCollection.find():
@@ -245,7 +248,10 @@ class User:
 				 	speakWelcome(user['name'], time() - float(self.last_online))
 				 	for i in range(len(pb)):
 					 	pb[i].push_note("Alfr3d", user['name']+" just came online")
-				usersCollection.update({"name":user['name']},{"$set":{'state':'online'}})
+				usersCollection.update({"name":user['name']},{"$set":{'state':'online',															   "location":{"name":cur_env['name'],
+																		   "city":cur_env['city'],
+																		   "state":cur_env['state'],
+																		   "country":cur_env['country']}}})
 			else:
 				if self.state == "online":
 					for i in range(len(pb)):
