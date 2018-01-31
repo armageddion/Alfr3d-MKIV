ce51031e32ca180f5cfce9a149f1bda0a1aa9ba5
Armageddion <armageddion@gmail.com>
2018-01-30 23:09:26 -0500

    continued work on web ui`

--- alfr3d.io/js/main.js
+++ alfr3d.io/js/main.js
@@ -7,6 +7,7 @@ var controls;
 
 var alfr3d_blue = 0x33b5e5;
 var alfr3d_orange = 0xFF8400;
+var alfr3d_black = 0x071a21;
 var alfr3d_dot = 0xFF194C;
 
 // create 2D array grid*grid large
@@ -106,14 +107,22 @@ function init() {
     //cust_geo.computeBoundingSphere();
     //cust_geo.computeFaceNormals();
 
-    cust_geo_mat = new THREE.MeshBasicMaterial({ color: alfr3d_blue, transparent: true, opacity: 0.8 , side:THREE.DoubleSide});
+    cust_geo_mat = new THREE.MeshBasicMaterial({ color: alfr3d_black, 
+                                                 transparent: true, 
+                                                 opacity: 0.8, 
+                                                 side:THREE.DoubleSide});
     geo_mesh = new THREE.Mesh(cust_geo,cust_geo_mat);
     geo_mesh.position.set(-window.innerWidth*0.5,window.innerHeight*0.5,-50*cust_step);
     geo_mesh.callback = function() { console.log("clicked 1"); }
     scene.add(geo_mesh); 
 
-    var cust_geo2 = new THREE.Geometry();
+    var cust_geo_border = new THREE.EdgesGeometry(cust_geo);
+    cust_geo_border_mat = new THREE.LineBasicMaterial( { color: alfr3d_blue, linewidth: 10});
+    var cust_geo_frame = new THREE.LineSegments( cust_geo_border, cust_geo_border_mat);
+    cust_geo_frame.position.set(-window.innerWidth*0.5,window.innerHeight*0.5,-50*cust_step);
+    scene.add( cust_geo_frame );
 
+    var cust_geo2 = new THREE.Geometry();
 
     cust_geo2.vertices.push(
         new THREE.Vector3(0*cust_step/5,0*cust_step/5,0),
@@ -134,52 +143,54 @@ function init() {
     cust_geo2.faces.push(new THREE.Face3(0,5,6));
     cust_geo2.faces.push(new THREE.Face3(0,6,7));                
 
-    cust_geo_mat2 = new THREE.MeshBasicMaterial({ color: alfr3d_blue, 
+    cust_geo2_mat = new THREE.MeshBasicMaterial({ color: alfr3d_black, 
                                                   transparent: true, 
-                                                  opacity: 0.9, 
-                                                  side:THREE.DoubleSide});    
+                                                  opacity: 0.8, 
+                                                  side:THREE.DoubleSide});  
+    cust_geo2_border_mat = new THREE.LineBasicMaterial({ color: alfr3d_blue, 
+                                                         linewidth: 10});
     
-    geo_mesh2 = new THREE.Mesh(cust_geo2,cust_geo_mat2);
-    geo_mesh2.position.set(-window.innerWidth*0.5+100,window.innerHeight*0.5+50,-40*cust_step);
+    geo_mesh2 = new THREE.Mesh(cust_geo2,cust_geo2_mat);
+    geo_mesh2.position.set(-window.innerWidth*0.5+100,window.innerHeight*0.5+20,-40*cust_step);
     geo_mesh2.callback = function() { console.log("clicked 2"); }
+    var cust_geo2_border = new THREE.EdgesGeometry(cust_geo2);
+    var cust_geo2_frame = new THREE.LineSegments(cust_geo2_border,cust_geo2_border_mat);
+    cust_geo2_frame.position.set(-window.innerWidth*0.5+100,window.innerHeight*0.5+20,-40*cust_step);
     scene.add(geo_mesh2);
+    scene.add(cust_geo2_frame);
     
-    geo_mesh3 = new THREE.Mesh(cust_geo2,cust_geo_mat2);
-    geo_mesh3.position.set(-window.innerWidth*0.5+220,window.innerHeight*0.5+50,-40*cust_step);
+    geo_mesh3 = new THREE.Mesh(cust_geo2,cust_geo2_mat);
+    geo_mesh3.position.set(-window.innerWidth*0.5+220,window.innerHeight*0.5+20,-40*cust_step);
     geo_mesh3.callback = function() { console.log("clicked 3"); }
+    var cust_geo3_border = new THREE.EdgesGeometry(cust_geo2);
+    var cust_geo3_frame = new THREE.LineSegments(cust_geo3_border,cust_geo2_border_mat);
+    cust_geo3_frame.position.set(-window.innerWidth*0.5+220,window.innerHeight*0.5+20,-40*cust_step);
     scene.add(geo_mesh3);    
+    scene.add(cust_geo3_frame);
 
-    geo_mesh4 = new THREE.Mesh(cust_geo2,cust_geo_mat2);
-    geo_mesh4.position.set(-window.innerWidth*0.5+340,window.innerHeight*0.5+50,-40*cust_step);
+    geo_mesh4 = new THREE.Mesh(cust_geo2,cust_geo2_mat);
+    geo_mesh4.position.set(-window.innerWidth*0.5+340,window.innerHeight*0.5+20,-40*cust_step);
     geo_mesh4.callback = function() { console.log("clicked 4"); }
+    var cust_geo4_border = new THREE.EdgesGeometry(cust_geo2);
+    var cust_geo4_frame = new THREE.LineSegments(cust_geo4_border,cust_geo2_border_mat);
+    cust_geo4_frame.position.set(-window.innerWidth*0.5+340,window.innerHeight*0.5+20,-40*cust_step);
     scene.add(geo_mesh4);
+    scene.add(cust_geo4_frame);
 
-    geo_mesh5 = new THREE.Mesh(cust_geo2,cust_geo_mat2);
-    geo_mesh5.position.set(-window.innerWidth*0.5+460,window.innerHeight*0.5+50,-40*cust_step);
+    geo_mesh5 = new THREE.Mesh(cust_geo2,cust_geo2_mat);
+    geo_mesh5.position.set(-window.innerWidth*0.5+460,window.innerHeight*0.5+20,-40*cust_step);
     geo_mesh5.callback = function() { console.log("clicked 5"); }
+    var cust_geo5_border = new THREE.EdgesGeometry(cust_geo2);
+    var cust_geo5_frame = new THREE.LineSegments(cust_geo5_border,cust_geo2_border_mat);
+    cust_geo5_frame.position.set(-window.innerWidth*0.5+460,window.innerHeight*0.5+20,-40*cust_step);
     scene.add(geo_mesh5);
-
-    var cust_geo3 = new THREE.EdgesGeometry(cust_geo2);
-    cust_geo_mat22 = new THREE.LineBasicMaterial( { color: alfr3d_blue, linewidth: 4 } );
-    var cust_geo_frame = new THREE.LineSegments( cust_geo3, cust_geo_mat22 );
-    cust_geo_frame.position.set(-window.innerWidth*0.5+400,-window.innerHeight*0.5+80,-40*cust_step);
-    scene.add( cust_geo_frame );
+    scene.add(cust_geo5_frame);
 
     // text play
-    var loader = new THREE.FontLoader();
-    loader.load( 'fonts/a3.otf', function ( font ) {
+    var label1 = makeTextSprite("status");
+    label1.position.set(-window.innerWidth*0.5+520,window.innerHeight*0.5,-39*cust_step);
+    scene.add(label1);
 
-        var geometry = new THREE.TextGeometry( 'Hello three.js!', {
-            font: font,
-            size: 80,
-            height: 5,
-            curveSegments: 12,
-            bevelEnabled: true,
-            bevelThickness: 10,
-            bevelSize: 8,
-            bevelSegments: 5
-        } );
-    } );    
 
     renderer = new THREE.WebGLRenderer({alpha: true, antialias: true});
     renderer.setSize( window.innerWidth, window.innerHeight );
@@ -252,4 +263,41 @@ function onWindowResize() {
     renderer.setSize( window.innerWidth, window.innerHeight );
 
     render();
+}
+
+function makeTextSprite( message, parameters )
+{
+    if ( parameters === undefined ) parameters = {};
+    var fontface = parameters.hasOwnProperty("fontface") ? parameters["fontface"] : "Arial";
+    var fontsize = parameters.hasOwnProperty("fontsize") ? parameters["fontsize"] : 120;
+    var borderThickness = parameters.hasOwnProperty("borderThickness") ? parameters["borderThickness"] : 4;
+    var borderColor = parameters.hasOwnProperty("borderColor") ?parameters["borderColor"] : { r:0, g:0, b:0, a:1.0 };
+    var backgroundColor = parameters.hasOwnProperty("backgroundColor") ?parameters["backgroundColor"] : { r:255, g:255, b:255, a:1.0 };
+    var textColor = parameters.hasOwnProperty("textColor") ?parameters["textColor"] : { r:255, g:255, b:255, a:1.0 };
+
+    var canvas = document.createElement('canvas');
+    canvas.width = 500;
+    canvas.heaight = 100;
+    var context = canvas.getContext('2d');
+    context.font = "Bold " + fontsize + "px " + fontface;
+    var metrics = context.measureText( message );
+    var textWidth = metrics.width;
+
+    context.fillStyle   = "rgba(" + backgroundColor.r + "," + backgroundColor.g + "," + backgroundColor.b + "," + backgroundColor.a + ")";
+    context.strokeStyle = "rgba(" + borderColor.r + "," + borderColor.g + "," + borderColor.b + "," + borderColor.a + ")";
+
+    context.lineWidth = borderThickness;
+    //roundRect(context, borderThickness/2, borderThickness/2, (textWidth + borderThickness) * 1.1, fontsize * 1.4 + borderThickness, 8);
+
+    context.fillStyle = "rgba("+textColor.r+", "+textColor.g+", "+textColor.b+", 1.0)";
+    context.fillText( message, borderThickness, fontsize + borderThickness);
+
+    var texture = new THREE.Texture(canvas) 
+    texture.needsUpdate = true;
+
+    var spriteMaterial = new THREE.SpriteMaterial( { map: texture, useScreenCoordinates: false } );
+    var sprite = new THREE.Sprite( spriteMaterial );
+    //sprite.scale.set(0.5 * fontsize, 0.25 * fontsize, 0.75 * fontsize);
+    sprite.scale.set(100, 50, 1);
+    return sprite;  
 }
